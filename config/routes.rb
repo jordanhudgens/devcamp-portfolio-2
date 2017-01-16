ab9e45fb1b57c2ea6c151f267c4d046bcadaf335
cmckee-dev <cmckee.dev@gmail.com>
2017-01-15 21:47:47 -0700

    Updating Devise config and adding devise Users
    - Added action_mailer localhost config
    - Ran devise views generator
    - Ran devise User generator
    - Updated User with name attribute

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
   get 'angular-items', to: 'portfolios#angular'
+  
+  devise_for :users
 end
